04557c0351e4fc61bb7a72b1c89554a3136e4f50
bwalden1 <64103801+bwalden1@users.noreply.github.com>
2021-07-16 21:34:03 -0400

    stuff

---
+++ DotNetCoreApi/Controllers/BaseController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<T> : ControllerBase where T : class
+    {
+        // GET: api/<CRUDController>
+        [HttpGet]
+        public IEnumerable<T> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CRUDController>/5
+        [HttpGet("{id}")]
+        public T Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CRUDController>
+        [HttpPost]
+        public void Post([FromBody] T record)
+        {
+        }
+
+        // PUT api/<CRUDController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CRUDController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DotNetCoreApi/Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreApi.Repository
+{
+    public class Repository<T>  where T : class 
+    {
+        private readonly string _connectionString;
+
+        public Repository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private SqlConnection SqlConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        private IDbConnection CreateConnection()
+        {
+            var conn = SqlConnection();
+
+            return conn;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            using (var connection = CreateConnection())
+            {
+                return await connection.GetListAsync<T>();
+            }
+        }   
+
+        public async Task<IEnumerable<T>> GetAllAsync(object where)
+        {
+            using (var connection = CreateConnection())
+            {
+                return await connection.GetListAsync<T>(where);
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(string conditions, object param)
+        {
+            using (var connection = CreateConnection())
+            {
+                return await connection.GetListAsync<T>(conditions, param);
+            }
+        }
+    }
+}
